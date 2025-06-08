20c81f18bcc977d09503c294e767dcb758024b6a
suulayman5 <ajahsuleiman@gmail.com>
2025-06-08 01:17:18 +0100

    player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float moveSpeed = 3f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        Debug.Log(inputVector);
+    }
+}
